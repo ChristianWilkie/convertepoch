20515547d83829ec57bf4633f07fb0ca5ed57c1e
Christian Wilkie <christian.wilkie@gmail.com>
2021-04-04 01:48:30 -0400

    days since epoch

--- src/app/day/day.component.ts
+++ src/app/day/day.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./day.component.scss']
 })
 export class DayComponent implements OnInit {
+  private readonly timeSinceEpoch: number;
+  public fullDaysSinceEpoch: number;
+  public dateString: string;
 
-  constructor() { }
+  constructor() {
+    this.timeSinceEpoch = new Date().getTime();
+    this.fullDaysSinceEpoch = this.getFullDaysSinceEpoch(this.timeSinceEpoch);
+    this.dateString = new Date(this.timeSinceEpoch).toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+  }
 
   ngOnInit(): void {
   }
 
+  getFullDaysSinceEpoch(time: number): number {
+    return Math.floor(time / 8.64e7);
+  }
 }
